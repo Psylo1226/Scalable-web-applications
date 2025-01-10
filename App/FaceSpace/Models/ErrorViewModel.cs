daf9f04e3ea835cb8ae7fd857a48fa2441e93e98
Piotr Stawarski <staw2000@gmail.com>
2025-01-10 16:01:56 +0100

    initial

---
+++ App/FaceSpace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FaceSpace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
