b331ef20086940c6db8ad174a98f1794eb3dbd3d
Piotr Stawarski <staw2000@gmail.com>
2025-01-22 22:42:03 +0100

    stash

---
+++ App/FaceSpace/Controllers/ProfileController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FaceSpace.Models;
+
+namespace FaceSpace.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new UserProfileViewModel(); // Tworzenie modelu
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserProfileViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Możesz tutaj zapisać zmiany do bazy danych
+                TempData["SuccessMessage"] = "Profile updated successfully!";
+            }
+
+            // Jeśli coś poszło nie tak, odtwórz listę awatarów
+            model.AvatarOptions = new List<string>
+            {
+                "/img/avatar1.png",
+                "/img/avatar2.png",
+                "/img/avatar3.png",
+                "/img/avatar4.png"
+            };
+
+            return View(model);
+        }
+    }
+}
